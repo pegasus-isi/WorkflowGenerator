4926737ba20219a6847d4350dfd4758275b53b82
Gideon Juve <juve@usc.edu>
2011-12-12 10:50:16 -0800

    Add DOT generation routine

--- workflow.py
+++ workflow.py
@@ -8,6 +8,30 @@ SECONDS = 1
 MINUTES = 60*SECONDS
 HOURS = 60*MINUTES
 
+COLORS = [
+        "#1b9e77",
+        "#d95f02",
+        "#7570b3",
+        "#e7298a",
+        "#66a61e",
+        "#e6ab02",
+        "#a6761d",
+        "#666666",
+        "#8dd3c7",
+        "#bebada",
+        "#fb8072",
+        "#80b1d3",
+        "#fdb462",
+        "#b3de69",
+        "#fccde5",
+        "#d9d9d9",
+        "#bc80bd",
+        "#ccebc5",
+        "#ffed6f",
+        "#ffffb3"
+]
+
+
 class File:
     def __init__(self, name, size=0):
         self.name = name
@@ -72,11 +96,40 @@ class Workflow:
                     f.write('\t\t<parent ref="%s">\n' % (p.id))
                 f.write('\t</child>\n')
         
-        f.write('</adag>')
+        f.write('</adag>\n')
         f.close()
     
-    def writeDOT(self, filename):
-        raise Exception("Not implemented")
+    def writeDOT(self, filename, width=8.0, height=10.0):
+        next_color = 0  # Keep track of next color
+        xforms = {} # Keep track of transformation names to assign colors
+        
+        f = open(filename,'w')
+        
+        f.write("""digraph dag {
+    size="%s,%s"
+    ratio=fill
+    node [shape=ellipse, style=filled]
+    edge [arrowhead=normal, arrowsize=1.0]
+    \n""" % (width,height))
+        
+        for j in self.jobs:
+            if j.name not in xforms:
+                xforms[j.name] = next_color
+                next_color += 1
+                # Just in case we run out of colors
+                next_color = min(len(COLORS)-1, next_color)
+            color = xforms[j.name]
+            f.write('\t"%s" [color="%s",label="%s"]\n' % (j.id,COLORS[color],j.name))
+        
+        f.write('\n')
+        
+        for j in self.jobs:
+            for p in j.parents:
+                f.write('\t"%s" -> "%s"\n' % (p.id, j.id))
+        
+        f.write("}\n")
+        f.close()
+
 
 if __name__ == '__main__':
     w = Workflow(name="Test", description="""Test Workflow""")
@@ -91,3 +144,4 @@ if __name__ == '__main__':
     w.addJob(j2)
     
     w.writeDAX("/dev/stdout")
+    w.writeDOT("/dev/stdout")
