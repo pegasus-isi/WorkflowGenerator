462244f6b32e10455400ba632cbd9bdc0e48a2d7
Gideon Juve <juve@usc.edu>
2011-12-12 10:27:35 -0800

    Add code to call all generators

---
+++ all.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    import leadmm
+    import leadadas
+    import leaddm
+    import scoop
+    import floodplain
+    
+    leadmm.main("leadmm.dax")
+    leadadas.main("leadadas.dax")
+    leaddm.main("leaddm.dax")
+    scoop.main("scoop.dax")
+    floodplain.main("floodplain.dax")
